1d261ce7997e98e646cc8865f5ceffa981cff396
Engin Gülek <engingulek0@gmail.com>
2021-10-20 00:00:54 +0300

    AddAdvertPageDesign

---
+++ AdoptingHay/AdoptingHay/View/AddAdvertVC.swift
@@ -0,0 +1,36 @@
+//
+//  AddAdvertVC.swift
+//  AdoptingHay
+//
+//  Created by engin gülek on 19.10.2021.
+//
+
+import UIKit
+
+class AddAdvertVC: UIViewController {
+
+    @IBOutlet weak var addAdvertImageView: UIImageView!
+    
+    @IBOutlet weak var addAdvertNameTextField: UITextField!
+    @IBOutlet weak var addAdvertAgeTextField: UITextField!
+    
+    @IBOutlet weak var addAdvertKindsTextField: UITextField!
+    
+    @IBOutlet weak var addAdvertSickTextField: UITextField!
+    
+    @IBOutlet weak var addAdvertDescTextView: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    @IBAction func addAdvertButton(_ sender: Any) {
+    }
+    
+    
+
+}
