bc451fb3d07c0d6e614f45ef8fd63faf6aff7d89
Engin Gülek <engingulek0@gmail.com>
2021-10-29 20:35:16 +0300

    AccountPageDesignAndSingOut

---
+++ AdoptingHay/AdoptingHay/View/AccountTVC.swift
@@ -0,0 +1,25 @@
+//
+//  AccountTVC.swift
+//  AdoptingHay
+//
+//  Created by engin gülek on 29.10.2021.
+//
+
+import UIKit
+
+class AccountTVC: UITableViewCell {
+
+    @IBOutlet weak var imageIcon: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- AdoptingHay/AdoptingHay/View/AccountVC.swift
+++ AdoptingHay/AdoptingHay/View/AccountVC.swift
@@ -6,38 +6,31 @@
 //
 
 import UIKit
+import Firebase
 
-class AccountVC: UIViewController {
+class AccountVC: UIViewController{
+ 
+  
+    @IBOutlet weak var tableView: UITableView!
+    
 
+    let accountPageList = ["Hesap Bilgilerim","Favorilerim","Geçmiş Sahiplenme"]
+    
+    let accountPageListImage = ["gearshape","suit.heart.fill","link"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        super.viewDidLoad()
-               
-                // view Design
-        let myNewView=UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.height/4.5))
-            myNewView.backgroundColor=UIColor.red
-        myNewView.layer.zPosition = -1
-        self.view.addSubview(myNewView)
-        
-        // NameLabelDesign
-        let  myNameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 1220, height: 30))
-        myNameLabel.text = "Engin Gülek"
-        myNameLabel.textAlignment = .center
-        myNameLabel.center.x = self.view.center.x
-        myNameLabel.center.y = self.view.center.y/4.5
-        myNameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        myNameLabel.textColor = .white
-   
+        tableView.delegate = self
+        tableView.dataSource = self
        
-
-    
-            
-    self.view.addSubview(myNameLabel)
-    
+       
         
+        
+        
+    
+        design()
         
 
       
@@ -45,7 +38,78 @@ class AccountVC: UIViewController {
     
     }
     
+    func design() {
+        // view Design
+let myNewView=UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.height/4.5))
+    myNewView.backgroundColor=UIColor.red
+myNewView.layer.zPosition = -1
+self.view.addSubview(myNewView)
+
+// NameLabelDesign
+let  myNameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 1220, height: 30))
+myNameLabel.text = "Engin Gülek"
+myNameLabel.textAlignment = .center
+myNameLabel.center.x = self.view.center.x
+myNameLabel.center.y = self.view.center.y/4.5
+myNameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+myNameLabel.textColor = .white
+self.view.addSubview(myNameLabel)
+
+//AccountImage
+let personImage = UIImage(named: "paw.png")
+let myImageView:UIImageView = UIImageView()
+myImageView.contentMode = UIView.ContentMode.scaleAspectFit
+myImageView.frame.size.width = self.view.frame.width/4
+myImageView.frame.size.height = self.view.frame.height/8
+myImageView.center.x = self.view.center.x
+myImageView.center.y = self.view.center.y/2.5
+myImageView.image = personImage
+myImageView.backgroundColor = .white
+myImageView.layer.borderColor = UIColor.red.cgColor
+myImageView.layer.borderWidth = 3
+myImageView.layer.cornerRadius = 15
+view.addSubview(myImageView)
+        
+    }
+    
 
  
-
+    @IBAction func exitAccountButton(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+     do {
+       try firebaseAuth.signOut()
+         performSegue(withIdentifier: "exitToSingPage", sender: nil)
+     } catch let signOutError as NSError {
+       print("Error signing out: %@", signOutError)
+     }
+        
+    }
+    
 }
+
+
+
+extension  AccountVC :  UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return accountPageList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "accontCell", for: indexPath) as! AccountTVC
+        cell.label.text = accountPageList[indexPath.row]
+        cell.imageIcon.image = UIImage(systemName: "\(accountPageListImage[indexPath.row])")
+        cell.layer.cornerRadius = 15
+        
+    
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(accountPageList[indexPath.row])
+        performSegue(withIdentifier: "accountPagetoS", sender: nil)
+        self.tableView.isHidden = false
+    }
+    
+    
+}
+
+
