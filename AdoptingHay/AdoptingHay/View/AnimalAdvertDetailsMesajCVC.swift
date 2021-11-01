c4562c4ba0b3fcf7c6f03eca058a1882e359abf8
Engin Gülek <engingulek0@gmail.com>
2021-11-01 23:15:22 +0300

    MessageSendFirstAction

---
+++ AdoptingHay/AdoptingHay/View/AnimalAdvertDetailsMesajCVC.swift
@@ -0,0 +1,13 @@
+//
+//  AnimalAdvertDetailsCell.swift
+//  AdoptingHay
+//
+//  Created by engin gülek on 1.11.2021.
+//
+
+import UIKit
+
+class AnimalAdvertDetailsMesajCVC: UICollectionViewCell {
+    @IBOutlet weak var mesajLabel: UILabel!
+    
+}
--- AdoptingHay/AdoptingHay/View/AnimalAdvertDetailsVC.swift
+++ AdoptingHay/AdoptingHay/View/AnimalAdvertDetailsVC.swift
@@ -14,8 +14,13 @@ class AnimalAdvertDetailsVC: UIViewController {
     
     @IBOutlet weak var animalDetailsImagesCollectionView: UICollectionView!
     
+    @IBOutlet weak var animalAdvertDetailMesajCollectionView: UICollectionView!
+    
+    @IBOutlet weak var mesajText: UITextField!
+    
     
     var animalImages:[String] = [String]()
+    var autoMesaj:[String] = [String]()
 
     
     override func viewDidLoad() {
@@ -23,10 +28,22 @@ class AnimalAdvertDetailsVC: UIViewController {
         
         animalDetailsImagesCollectionView.delegate = self
         animalDetailsImagesCollectionView.dataSource = self
+        
+        
+        
+        animalAdvertDetailMesajCollectionView.delegate = self
+        animalAdvertDetailMesajCollectionView.dataSource = self
+        
+        
         animalDetailsImagesCollectionView.showsHorizontalScrollIndicator = false
+        animalAdvertDetailMesajCollectionView.showsHorizontalScrollIndicator = false
         
         animalImages = ["pamukkopek","tarcinkedi","sarikus"]
+        autoMesaj = ["Merhaba","İlan Akifmi","Durumu nasıl","Sahiplencem"]
         
+        
+        
+        // mesajcell
         
         
         let  myNameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 1220, height: 30))
@@ -51,6 +68,17 @@ class AnimalAdvertDetailsVC: UIViewController {
     }
     
 
+    @IBAction func messageSend(_ sender: Any) {
+        
+        if mesajText.text == "" {
+           print("Text Boş")
+        }
+        
+        else {
+            print(mesajText.text ?? "Hata")
+        }
+       
+    }
     
     
 
@@ -61,11 +89,20 @@ class AnimalAdvertDetailsVC: UIViewController {
 
 extension AnimalAdvertDetailsVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return animalImages.count
+        
+        if collectionView == self.animalDetailsImagesCollectionView {
+            return animalImages.count
+        }
+        
+        
+        return autoMesaj.count
+       
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-       
+        
+        
+        if collectionView == self.animalDetailsImagesCollectionView {
             let cell = animalDetailsImagesCollectionView.dequeueReusableCell(withReuseIdentifier: "animalDetailsImageCell", for: indexPath) as! AnimalAdvertDetailsImageCVC
         cell.animalmageDetails.image = UIImage(named: animalImages[indexPath.row])
         
@@ -74,12 +111,38 @@ extension AnimalAdvertDetailsVC: UICollectionViewDelegate, UICollectionViewDataS
         cell.layer.borderWidth = 2
         return cell
             
+        }
+       
+        else {
+            let cell = animalAdvertDetailMesajCollectionView.dequeueReusableCell(withReuseIdentifier: "mesajcell", for: indexPath) as! AnimalAdvertDetailsMesajCVC
+            cell.mesajLabel.text = autoMesaj[indexPath.row]
+            cell.layer.cornerRadius = 25
+     
+            cell.layer.borderWidth = 2
+            cell.layer.borderColor = UIColor.red.cgColor
+            cell.backgroundColor = UIColor.red
+            
         
-        
-    
-        
-  
+            return cell
+            }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if collectionView == self.animalDetailsImagesCollectionView {
+        
+            
+        }
+       
+        else {
+            mesajText.text = autoMesaj[indexPath.row]
+            
+            }
+    }
+    
+    
+    
+    
     
     
 }
