16e165ed277d2da3c09690b69564805e1eddd795
Engin Gülek <engingulek0@gmail.com>
2021-12-23 13:41:17 +0300

    CreateMessageKit

--- AdoptingHay/AdoptingHay/View/DogWalking/Controller/DogChatVC.swift
+++ AdoptingHay/AdoptingHay/View/DogWalking/Controller/DogChatVC.swift
@@ -11,7 +11,7 @@ import InputBarAccessoryView
 import Firebase
 
 
-struct Message : MessageType {
+struct DogMessage : MessageType {
     var sender: SenderType
     
     var messageId: String
@@ -22,7 +22,8 @@ struct Message : MessageType {
 }
 
 
-struct Sender:SenderType {
+struct DogSender:SenderType {
+   
     var senderId: String
     var displayName: String
     
@@ -33,14 +34,71 @@ struct Sender:SenderType {
 }
 
 class DogChatVC: MessagesViewController {
+    private var messages = [Message]()
+    var userId:String?
+    private var selfSender = Sender(senderId: "1", displayName: "Engin Gülek")
+    private var sendSender = Sender(senderId: "2", displayName: "Selin Çiçek")
+    var messageViewModelList : MessageViewModelList!
+    var timer = Timer()
+    var  messageGetUserName : String?
+    var senderName:String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.tabBarController?.tabBar.isHidden = true
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+
+        
     }
     
 
    
 
+}
+
+
+
+extension DogChatVC : InputBarAccessoryViewDelegate {
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        print("\(text)")
+        
+    }
+    
+}
+
+
+extension DogChatVC :  MessagesDataSource ,MessagesLayoutDelegate, MessagesDisplayDelegate {
+   
+    
+    func currentSender() -> SenderType {
+      return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        
+       
+        return  messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        print("Message sayi \(messages.count)")
+        return messages.count
+    }
+    
+    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+        let sender = message.sender
+        if sender.senderId == selfSender.senderId {
+            // our image
+            avatarView.image = UIImage(named: "red")
+        }
+        else {
+            avatarView.image = UIImage(named: "blue")
+        }
+    }
+    
+    
 }
